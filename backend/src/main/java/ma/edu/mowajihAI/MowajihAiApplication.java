6470a4848e268661e834b992987e2a9baa7a69c1
AymanElMikh <aymanmikh7@gmail.com>
2025-01-30 17:00:13 +0100

    Spring Backend Intialized !

---
+++ backend/src/main/java/ma/edu/mowajihAI/MowajihAiApplication.java
@@ -0,0 +1,13 @@
+package ma.edu.mowajihAI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MowajihAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MowajihAiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/ma/edu/mowajihAI/MowajihAiApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.edu.mowajihAI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MowajihAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
